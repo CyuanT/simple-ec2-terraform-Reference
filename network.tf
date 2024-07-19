ce34b980a88b71c62649bace6b085fd50132ae6c
luqmannnn <luqmannurhakimt@gmail.com>
2024-07-19 22:39:15 +0800

    Update repository with terraform files for ec2

---
+++ network.tf
@@ -0,0 +1,47 @@
+resource "aws_security_group" "ec2_sg" {
+  name   = var.sg_name
+  vpc_id = data.aws_vpc.selected_vpc.id # var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+data "aws_vpc" "selected_vpc" {
+  filter {
+    name   = "tag:Name"
+    values = [var.vpc_name]
+  }
+}
+
+data "aws_subnet" "selected_subnet" {
+  filter {
+    name   = "tag:Name"
+    values = [var.subnet_name]
+  }
+}
